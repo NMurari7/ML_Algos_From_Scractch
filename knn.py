cf908e28c7893af0fa0d2003ea86a5485cc021a4
NMurari7 <70143030+NMurari7@users.noreply.github.com>
2022-11-07 21:11:38 +0530

    Add files via upload
    1) KNN algorithm from scratch

---
+++ knn.py
@@ -0,0 +1,72 @@
+import numpy as np
+from collections import Counter
+
+
+def euclidean_distance(x, y):
+    return np.sqrt(np.sum(x - y) ** 2)
+
+
+class KNN:
+    def __init__(self, k=3):
+        self.k = k
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+        pass
+
+    # def predict(self, x):
+    #     self.predicted = []
+    #     self.distances = []
+    #     for i in x:
+    #         res = []
+    #         for j in self.X_train:
+    #             res.append(self.euclidean_distance(i, j))
+    #         self.distances.append(res)
+    #     # print("Distances ", self.distances)
+    #
+    #     k_idx = []
+    #     for i in self.distances:
+    #         k_idx.append(np.argsort(i)[: self.k])
+    #     # print("index's", k_idx)
+    #
+    #     for i in k_idx:
+    #         labels = []
+    #         for j in i:
+    #             labels.append(self.y_train[j])
+    #         # k_labels.append(labels)
+    #         common = Counter(labels).most_common(1)
+    #         self.predicted.append(common[0][0])
+    #     # print("Predicted ", self.predicted)
+    #     return self.predicted
+
+
+    def predict(self, X):
+        y_pred = [self._predict(x) for x in X]
+        return np.array(y_pred)
+
+    def _predict(self, x):
+        # Compute distances between x and all examples in the training set
+        distances = [euclidean_distance(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the first k neighbors
+        k_idx = np.argsort(distances)[: self.k]
+        # Extract the labels of the k nearest neighbor training samples
+        k_neighbor_labels = [self.y_train[i] for i in k_idx]
+        # return the most common class label
+        most_common = Counter(k_neighbor_labels).most_common(1)
+        return most_common[0][0]
+
+if __name__ == '__main__':
+    from sklearn import datasets
+    from sklearn.metrics import accuracy_score
+    from sklearn.model_selection import train_test_split
+
+    iris = datasets.load_iris()
+    X, y = iris.data, iris.target
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+    k = 3
+    clf = KNN(k)
+    clf.fit(X_train, y_train)
+    predictions = clf.predict(X_test)
+    print("KNN classification accuracy", accuracy_score(y_test, predictions))
